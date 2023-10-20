158b8eceb1104ec2cc1a57e631da696d92876148
EliasAraujoDev <eliasaraujo1222@outlook.com>
2023-10-19 23:18:28 -0300

    werewr

--- m_v/Scripts/activate.bat
+++ m_v/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\aluno.SENACLAB\Music\elias_tec_informatica_manha\git\estudodeFlaskGET_POST\m_v
+set VIRTUAL_ENV=C:\Users\elias\OneDrive\Documentos\Codigos\codigos\estudodeFlaskGET_POST\m_v
 
 if not defined PROMPT set PROMPT=$P$G
 
